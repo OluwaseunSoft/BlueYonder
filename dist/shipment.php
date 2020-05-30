c8a737f1d600f502b59232e2910f630497b0fa07
Oluwaseun <oluwaseunazure@yahoo.com>
2020-05-30 13:02:50 +0100

    javascript for new file

--- dist/shipment.php
+++ dist/shipment.php
@@ -15,7 +15,7 @@ body {
     color: #000;
     overflow-x: hidden;
     height: 100%;
-    background: linear-gradient(-45deg, #2196F3 50%, #EEEEEE 50%);
+    /* background: linear-gradient(-45deg, #2196F3 50%, #EEEEEE 50%); */
     background-repeat: no-repeat
 }
 
@@ -323,11 +323,85 @@ button {
                         <h1 class="mt-4">Static Navigation</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Static Navigation</li>
+                            <li class="breadcrumb-item active">Shipment</li>
                         </ol>
                         <div class="card mb-4">
                             <div class="card-body">
-                                <p class="mb-0">This page is an example of using static navigation. By removing the <code>.sb-nav-fixed</code> class from the <code>body</code>, the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.</p>
+                            <div class="container-fluid px-1 py-5 mx-auto">
+    <div class="row d-flex justify-content-center">
+        <div class="col-xl-5 col-lg-6 col-md-7">
+            <div class="card b-0">
+                <h3 class="heading">Create A Shipment</h3>
+                <!-- <p class="desc">Fill out the form or call <span class="yellow-text">123 456 7891</span><br>to start protecting your business today!</p> -->
+                <ul id="progressbar" class="text-center">
+                    <li class="active step0" id="step1"></li>
+                    <li class="step0" id="step2"></li>
+                    <li class="step0" id="step3"></li>
+                    <li class="step0" id="step4"></li>
+                </ul>
+                <fieldset class="show">
+                    <div class="form-card">
+                        <h5 class="sub-heading">Select Vehicle</h5>
+                        <div class="row px-1 radio-group">
+                            <div class="card-block text-center radio selected">
+                                <div class="image-icon"> <i class="fas fa-bus fa-5x"></i> </div>
+                                <p class="sub-desc">Van</p>
+                            </div>
+                            <div class="card-block text-center radio">
+                                <div class="image-icon"> <i class="fas fa-truck-moving fa-5x"></i> </div>
+                                <p class="sub-desc">Truck</p>
+                            </div>
+                            <div class="card-block text-center radio">
+                                <div class="image-icon"> <i class="fas fa-truck fa-5x"></i></i> </div>
+                                <p class="sub-desc">Mini Truck</p>
+                            </div>
+                            <div class="card-block text-center radio">
+                                <div class="image-icon"> <i class="fas fa-motorcycle fa-5x"></i> </div>
+                                <p class="sub-desc">Motor Bike</p>
+                            </div>
+                        </div> <button id="next1" class="btn-block btn-primary mt-3 mb-1 next">NEXT<span class="fas fa-long-arrow-alt-right"></span></button>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div class="form-card">
+                        <h5 class="sub-heading mb-4">Where Do You Want Us Pick Up Your Item(s)</h5> <label class="text-danger mb-3">* Required</label>
+                        <div class="form-group"> <label class="form-control-label">Pickup Location * :</label> <input type="text" id="fname" name="fname" placeholder="" class="form-control" onblur="validate1(1)"> </div>
+                        <div class="form-group"> <label class="form-control-label">Dropoff Location * :</label> <input type="text" id="lname" name="lname" placeholder="" class="form-control" onblur="validate1(2)"> </div>
+                        <div class="form-group"> <label class="form-control-label">Item Description * :</label> <input type="text" id="email" name="email" placeholder="" class="form-control" onblur="validate1(3)"> </div>
+                        <div class="form-group"> <label class="form-control-label">Reciever's Full Name * :</label> <input type="text" id="mob" name="mob" placeholder="" class="form-control" onblur="validate1(4)"> </div>
+                        <div class="form-group"> <label class="form-control-label">Reciever's Phone No. * :</label> <input type="text" id="RecieverPhone" name="RecieverPhone" placeholder="" class="form-control" onblur="validate1(5)"> </div>
+                        <button id="next2" class="btn-block btn-primary mt-3 mb-1 next mt-4" onclick="validate1(0)">NEXT<span class="fas fa-long-arrow-alt-right"></span></button> <button class="btn-block btn-secondary mt-3 mb-1 prev"><span class="fas fa-long-arrow-alt-left"></span>PREVIOUS</button>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div class="form-card">
+                        <h5 class="sub-heading mb-4">Company Details</h5> <label class="text-danger mb-3">* Required</label>
+                        <div class="form-group"> <label class="form-control-label">Company Name * :</label> <input type="text" id="cname" name="cname" placeholder="" class="form-control" onblur="validate2(1)"> </div>
+                        <div class="form-group"> <label class="form-control-label">Zip Code * :</label> <input type="text" id="zip" name="zip" placeholder="" class="form-control" onblur="validate2(2)"> </div>
+                        <div class="form-group"> <label class="form-control-label">State * :</label> <input type="text" id="state" name="state" placeholder="" class="form-control" onblur="validate2(3)"> </div>
+                        <div class="form-group"> <label class="form-control-label">City * :</label> <input type="text" id="city" name="city" placeholder="" class="form-control" onblur="validate2(4)"> </div>
+                        <div class="form-group"> <label class="form-control-label">Shredding Job Size * :</label>
+                            <div class="select mb-3"> <select name="shred-info" class="form-control">
+                                    <option>NA</option>
+                                    <option>1-20 boxes</option>
+                                    <option>20-50 boxes</option>
+                                    <option>50-100 boxes</option>
+                                    <option>100+ boxes</option>
+                                </select> </div>
+                        </div> <button id="next3" class="btn-block btn-primary mt-3 mb-1 next mt-4" onclick="validate2(0)">SUBMIT REQUEST<span class="fas fa-long-arrow-alt-right"></span></button> <button class="btn-block btn-secondary mt-3 mb-1 prev"><span class="fas fa-long-arrow-alt-left"></span>PREVIOUS</button>
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div class="form-card">
+                        <h5 class="sub-heading mb-4">Success!</h5>
+                        <p class="message">Thank You for choosing our website.<br>Quotation will be sent to your Email ID and Contact Number.</p>
+                        <div class="check"> <img class="fit-image check-img" src="https://i.imgur.com/QH6Zd6Y.gif"> </div>
+                    </div>
+                </fieldset>
+            </div>
+        </div>
+    </div>
+</div>
                             </div>
 
                         </div>
@@ -350,6 +424,219 @@ button {
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+        <script type='text/javascript'>
+function validate1(val) {
+v1 = document.getElementById("fname");
+v2 = document.getElementById("lname");
+v3 = document.getElementById("email");
+v4 = document.getElementById("mob");
+v5 = document.getElementById("RecieverPhone");
+
+flag1 = true;
+flag2 = true;
+flag3 = true;
+flag4 = true;
+flag5 = true;
+
+if(val>=1 || val==0) {
+if(v1.value == "") {
+v1.style.borderColor = "red";
+flag1 = false;
+}
+else {
+v1.style.borderColor = "green";
+flag1 = true;
+}
+}
+
+if(val>=2 || val==0) {
+if(v2.value == "") {
+v2.style.borderColor = "red";
+flag2 = false;
+}
+else {
+v2.style.borderColor = "green";
+flag2 = true;
+}
+}
+
+if(val>=3 || val==0) {
+if(v3.value == "") {
+v3.style.borderColor = "red";
+flag3 = false;
+}
+else {
+v3.style.borderColor = "green";
+flag3 = true;
+}
+}
+
+if(val>=4 || val==0) {
+if(v4.value == "") {
+v4.style.borderColor = "red";
+flag4 = false;
+}
+else {
+v4.style.borderColor = "green";
+flag4 = true;
+}
+}
+
+if(val>=5 || val==0) {
+if(v5.value == "") {
+v5.style.borderColor = "red";
+flag4 = false;
+}
+else {
+v5.style.borderColor = "green";
+flag5 = true;
+}
+}
+
+flag = flag1 && flag2 && flag3 && flag4 && flag5;
+
+return flag;
+}
+
+function validate2(val) {
+v1 = document.getElementById("cname");
+v2 = document.getElementById("zip");
+v3 = document.getElementById("state");
+v4 = document.getElementById("city");
+
+flag1 = true;
+flag2 = true;
+flag3 = true;
+flag4 = true;
+
+if(val>=1 || val==0) {
+if(v1.value == "") {
+v1.style.borderColor = "red";
+flag1 = false;
+}
+else {
+v1.style.borderColor = "green";
+flag1 = true;
+}
+}
+
+if(val>=2 || val==0) {
+if(v2.value == "") {
+v2.style.borderColor = "red";
+flag2 = false;
+}
+else {
+v2.style.borderColor = "green";
+flag2 = true;
+}
+}
+
+if(val>=3 || val==0) {
+if(v3.value == "") {
+v3.style.borderColor = "red";
+flag3 = false;
+}
+else {
+v3.style.borderColor = "green";
+flag3 = true;
+}
+}
+
+if(val>=4 || val==0) {
+if(v4.value == "") {
+v4.style.borderColor = "red";
+flag4 = false;
+}
+else {
+v4.style.borderColor = "green";
+flag4 = true;
+}
+}
+
+flag = flag1 && flag2 && flag3 && flag4;
+
+return flag;
+}
+
+$(document).ready(function(){
+
+var current_fs, next_fs, previous_fs;
+
+$(".next").click(function(){
+
+str1 = "next1";
+str2 = "next2";
+str3 = "next3";
+
+if(!str2.localeCompare($(this).attr('id')) && validate1(0) == true) {
+val2 = true;
+}
+else {
+val2 = false;
+}
+
+if(!str3.localeCompare($(this).attr('id')) && validate2(0) == true) {
+val3 = true;
+}
+else {
+val3 = false;
+}
+
+if(!str1.localeCompare($(this).attr('id')) || (!str2.localeCompare($(this).attr('id')) && val2 == true) || (!str3.localeCompare($(this).attr('id')) && val3 == true)) {
+current_fs = $(this).parent().parent();
+next_fs = $(this).parent().parent().next();
+
+$(current_fs).removeClass("show");
+$(next_fs).addClass("show");
+
+$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+current_fs.animate({}, {
+step: function() {
+
+current_fs.css({
+'display': 'none',
+'position': 'relative'
+});
+
+next_fs.css({
+'display': 'block'
+});
+}
+});
+}
+});
+
+$(".prev").click(function(){
+
+current_fs = $(this).parent().parent();
+previous_fs = $(this).parent().parent().prev();
+
+$(current_fs).removeClass("show");
+$(previous_fs).addClass("show");
+
+$("#progressbar li").eq($("fieldset").index(next_fs)).removeClass("active");
+
+current_fs.animate({}, {
+step: function() {
+
+current_fs.css({
+'display': 'none',
+'position': 'relative'
+});
+
+previous_fs.css({
+'display': 'block'
+});
+}
+});
+});
+
+$('.radio-group .radio').click(function(){
+$(this).toggleClass('selected');
+});
+
+});</script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
     </body>
