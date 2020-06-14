28d7d318748cde3009f1cdc95311d0a90417f8a5
Oluwaseun <oluwaseunazure@yahoo.com>
2020-06-14 21:00:16 +0100

    autocomplete input box

--- dist/shipment.php
+++ dist/shipment.php
@@ -437,6 +437,40 @@ function autocompleteSuggestionMapBoxAPI(inputParams, callback) {
                 callback(match);
             });
     }
+
+    function autocompleteInputBox(inp) {
+        var currentFocus;
+        inp.addEventListener("input", function (e) {
+            var a, b, i, val = this.value;
+            closeAllLists();
+            if (!val) {
+                return false;
+            }
+            currentFocus = -1;
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+            this.parentNode.appendChild(a);
+
+            // suggestion list MapBox api called with callback
+            autocompleteSuggestionMapBoxAPI($('#myInput').val(), function (results) {
+                results.features.forEach(function (key) {
+                    b = document.createElement("DIV");
+                    b.innerHTML = "<strong>" + key.place_name.substr(0, val.length) + "</strong>";
+                    b.innerHTML += key.place_name.substr(val.length);
+                    b.innerHTML += "<input type='hidden' data-lat='" + key.geometry.coordinates[1] + "' data-lng='" + key.geometry.coordinates[0] + "'  value='" + key.place_name + "'>";
+                    b.addEventListener("click", function (e) {
+                        let lat = $(this).find('input').attr('data-lat');
+                        let long = $(this).find('input').attr('data-lng');
+                        inp.value = $(this).find('input').val();
+                        $(inp).attr('data-lat', lat);
+                        $(inp).attr('data-lng', long);
+                        closeAllLists();
+                    });
+                    a.appendChild(b);
+                });
+            })
+        });
 </script>
                 <fieldset>
                     <div class="form-card">
